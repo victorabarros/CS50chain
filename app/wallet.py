beb7067a2bbb1d486e5c416754f57c6c3e9ee17d
VictorBarros <victorbarros1130@gmail.com>
2022-06-05 01:33:06 -0300

    set initial balance

--- app/wallet.py
+++ app/wallet.py
@@ -1,10 +1,21 @@
 import hashlib
 from datetime import datetime
 
+from transaction import Transaction
+
 
 class Wallet:
-    def __init__(self, pub_key):
+    initial_balance = 1000
+
+    def __init__(self, pub_key: str):
         self.created_at = datetime.utcnow()
+        # TODO improve how create pub and pem key (https://cryptography.io/en/latest/)
         self.public_key = pub_key
         self.private_key = hashlib.sha256(
-            f"{self.created_at}{self.public_key}".encode()).digest().hex()
+            f"{self.created_at}{self.public_key}".encode()).hexdigest()
+        self._set_initial_balance()
+
+    def _set_initial_balance(self):
+        trx = Transaction(self.public_key, self.private_key,
+                          self.initial_balance, "Initial balance")
+        # TODO send to blockchain
