a7eec68ae13b7fd10bee207af09beecf91230250
VictorBarros <victorbarros1130@gmail.com>
2022-06-06 22:17:02 -0300

    mine block method

--- app/app.py
+++ app/app.py
@@ -5,16 +5,21 @@ from node import Node
 
 if __name__ == "__main__":
     sender_wallet = Wallet()
+    print("sender done")
     recipient_wallet = Wallet()
-
-    trx = Transaction(sender_wallet.public_key,
-                      recipient_wallet.public_key, 50.2)
+    print("recipient done")
 
     node = Node()
 
-    trx.do_sign(sender_wallet.private_key)
-    node.submit_transaction(trx)
+    # trx = Transaction(sender_wallet.public_key,
+    #                   recipient_wallet.public_key, 50.2)
+    # trx.do_sign(sender_wallet.private_key)
+    # node.submit_transaction(trx)
 
+    print(node.get_chain())
+    print(node.get_transactions())
+    node.mine_block()
+    print(node.get_chain())
     print(node.get_transactions())
 
     # import pdb; pdb.set_trace()
--- app/node.py
+++ app/node.py
@@ -1,9 +1,13 @@
+import hashlib
 import jwt
+from datetime import datetime
+import json
 
 from config import ALGORITHM
 from transaction import Transaction
 
 _TRANSACTIONS = dict()
+_CHAIN = list()
 
 
 class Node:
@@ -14,21 +18,57 @@ class Node:
 
     @staticmethod
     def submit_transaction(transaction: Transaction):
-        if (not transaction.sign):
-            # TODO raise exception
-            pass
-
         jwt.decode(transaction.sign, transaction.sender_pub_key,
                    algorithms=[ALGORITHM])
 
-        _TRANSACTIONS.update({
-            transaction.sign: transaction
-        })
+        _TRANSACTIONS.update({transaction.sign: transaction})
 
     def get_transactions(self):
         return _TRANSACTIONS.values()
 
     def sync_node(self):
+        if len(_CHAIN) == 0:
+            _CHAIN.append({
+                'id': len(_CHAIN),
+                'created_at': datetime.utcnow(),
+                'data': None,
+                'nonce': None,
+                'hash': None,
+            })
         # TODO sync transactions and chain from other nodes
         # try using grpc https://grpc.io/docs/languages/python/basics/
-        pass
+
+    def get_chain(self):
+        return _CHAIN
+
+    def mine_block(self):
+        # self.sync_node()
+        new_block = {
+            'id': len(_CHAIN),
+            'created_at': datetime.utcnow(),
+            'data': {'transactions': list(_TRANSACTIONS.values())},
+            'nonce': self.proof_of_work(),
+            'hash': None,
+        }
+
+        new_block['hash'] = hashlib.sha256(json.dumps(
+            new_block, sort_keys=True, default=str).encode()).hexdigest()
+
+        _CHAIN.append(new_block)
+        _TRANSACTIONS.clear()
+
+        return new_block
+
+    def proof_of_work(self):
+        difficulty = 4
+        nonce = 0
+        previous_block_hash = _CHAIN[-1]['hash']
+
+        while True:
+            guess = (f'{previous_block_hash}{nonce}').encode()
+            guess_hash = hashlib.sha256(guess).hexdigest()
+            if guess_hash.startswith('0' * difficulty):
+                break
+            nonce += 1
+
+        return nonce
